bbf186d6efc3d7b8cd3d3804e7a3beb80e6ffb47
瓜田千風 <chikaze@Kenji-no-MacBook.local>
2020-08-06 18:16:31 +0900

    User name added

--- coin.java
+++ coin.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class coin{
     public static void main(String[] args){
+
+	Scanner scanner = new Scanner(System.in);
+	System.out.println("Who are you?");
+	String string = scanner.next();
+	System.out.println("Hello, " + string + "!");
 	
 	System.out.println("Tossing a coin...");
         int head = 0;
@@ -18,3 +25,25 @@ public class coin{
 	System.out.println("Heads: " + head + ", Tails: " + tail);
     }
 }
+
+/*public class coin{
+    public static void main(String[] args){
+	
+	System.out.println("Tossing a coin...");
+        int head = 0;
+	int tail = 0;
+	for(int i = 1;i <= 3; i++){
+	    int coin = new java.util.Random().nextInt(5);
+	    if(coin%2 == 0){
+		System.out.println("Round " + i + ": Heads");
+		head++;
+	    } else if(coin%2 == 1){
+		System.out.println("Round " + i + ": Tails");
+		tail++;
+	    }
+	}
+	
+	System.out.println("Heads: " + head + ", Tails: " + tail);
+    }
+    }*/
+// 一番最初
