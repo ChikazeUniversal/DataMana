83e2904bf35114f16f07420b8c0a7fe1c175a8c1
瓜田千風 <chikaze@Kenji-no-MacBook.local>
2020-08-06 18:29:59 +0900

    Victory message added

--- coin.java
+++ coin.java
@@ -24,8 +24,8 @@ public class coin{
 	
 	System.out.println("Heads: " + head + ", Tails: " + tail);
 
-	if(head > tail) System.out.println(string + " won");
-	else System.out.println(string + " lost");
+	if(head > tail) System.out.println("You won");
+	else System.out.println("You lost");
     }
 }
 
